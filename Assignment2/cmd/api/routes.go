b740e69a8b25ba4b943750dd5b2f2e9e73cf822c
Sultan <sultanahmullaev@mail.ru>
2023-11-23 15:26:13 +0600

    chapter 13

--- Assignment2/cmd/api/routes.go
+++ Assignment2/cmd/api/routes.go
@@ -20,5 +20,7 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodPatch, "/v1/sports/:id", app.updateSportHandler)
 	router.HandlerFunc(http.MethodDelete, "/v1/sports/:id", app.deleteSportHandler)
 
+	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
+
 	return app.recoverPanic(app.rateLimit(router))
 }
--- Assignment2/internal/data/models.go
+++ Assignment2/internal/data/models.go
@@ -12,10 +12,12 @@ var (
 
 type Models struct {
 	Sports SportsModel
+	Users  UserModel
 }
 
 func NewModels(db *sql.DB) Models {
 	return Models{
 		Sports: SportsModel{DB: db},
+		Users:  UserModel{DB: db},
 	}
 }
